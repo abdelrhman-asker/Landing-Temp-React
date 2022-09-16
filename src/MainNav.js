24745ae026f8d6b0043f8c2c81ee97d727c30fe1
unknown <abdosker11@yahoo.com>
2022-09-16 13:57:16 +0200

    nav changes

--- src/MainNav.js
+++ src/MainNav.js
@@ -17,7 +17,7 @@ function BasicExample() {
         <Navbar.Brand className='HashLinkMainLogo' href="#home">A-A Designs</Navbar.Brand>
         <Navbar.Toggle  aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <HashLink className='HashLink' to="/">Home</HashLink>
             <HashLink className='HashLink' to="/#Project">Project</HashLink>
             <HashLink className='HashLink' to="/#contact">Contact Us</HashLink>
